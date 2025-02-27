ddc0f0c22be70428218c23aa8b11a7f021359d97
Nobody <116659583+Tanzilya09@users.noreply.github.com>
2025-02-27 11:10:26 +0600

    Update 11.cpp

--- 11/11.cpp
+++ 11/11.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 struct Time {
-    int hours;   // часы
-    int minutes; // минуты
-    int seconds; // секунды
+    int hours;  
+    int minutes; 
+    int seconds; 
 };
 
-// Функция для перевода времени в секунды
 int toSeconds(const Time& t) {
     return t.hours * 3600 + t.minutes * 60 + t.seconds;
 }
 
-// Функция для перевода секунд обратно в формат часов, минут, секунд
 Time toTimeFormat(int totalSeconds) {
     Time t;
     t.hours = totalSeconds / 3600;
@@ -22,7 +20,6 @@ Time toTimeFormat(int totalSeconds) {
     return t;
 }
 
-// Функция для вывода времени в формате 12:59:59
 void printTime(const Time& t) {
     cout << t.hours << ":"
         << (t.minutes < 10 ? "0" : "") << t.minutes << ":"
@@ -32,25 +29,17 @@ void printTime(const Time& t) {
 int main() {
     Time t1, t2, result;
 
-    // Ввод первого времени
     cout << "Введите первое время (часы:минуты:секунды): ";
     cin >> t1.hours >> t1.minutes >> t1.seconds;
 
-    // Ввод второго времени
     cout << "Введите второе время (часы:минуты:секунды): ";
     cin >> t2.hours >> t2.minutes >> t2.seconds;
 
-    // Перевод обоих времен в секунды
     int totalSeconds1 = toSeconds(t1);
     int totalSeconds2 = toSeconds(t2);
 
-    // Складываем секунды
     int totalSeconds = totalSeconds1 + totalSeconds2;
-
-    // Переводим сумму секунд обратно в формат времени
     result = toTimeFormat(totalSeconds);
-
-    // Вывод результата
     cout << "Сумма времен: ";
     printTime(result);
 
